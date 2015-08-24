1e956b0e2ae7222a78528805080c0bd31041cca7
Matt Rixman <mrixman@econolite.com>
2015-08-24 17:59:37 -0600

    The trouble with evernote wasn't their servers, it was their client.  Using geeknote on either side eliminated the nonsense.  Updating personalize.sh accordingly.

--- personalize.sh
+++ personalize.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
+mkdir -p -m 700 $HOME/.ssh
+
 ## Contact evernote and fetch keys
 if [ ! -f "$HOME/.ssh/id_rsa" ]; then
-	geeknote find "Low Security Private Key" 
-	# Evernote escapes the -----'s and adds a newline, probably because it is interpreting them as markdown, stripping these out.
-	geeknote show 1 | sed '/^$/d' | sed 's/\\----/----/' | tail -n +8 > $HOME/.ssh/id_rsa
-	chmod 700 $HOME/.ssh/id_rsa
+	geeknote find "Blue Key (private)"
+	#Evernote escapes the ---- because it looks like markup, removing \'s
+	geeknote show 1 | tail -n 52 | head -n 51 | sed 's/\\----/----/' > $HOME/.ssh/id_rsa
+	chmod 600 $HOME/.ssh/id_rsa
 	echo "Fetched RSA private key"
 else
 	echo "RSA private key already present"
 fi
 
 if [ ! -f "$HOME/.ssh/id_rsa.pub" ]; then
-	geeknote find "Low Security Public Key"
-	# Evernote replaces mail@address.com with [mail@address.com](user@
-	geeknote show 1 | tail -n +9 | sed 's/\[[^]]*\](mailto:\([^)]*\))/\1/' > $HOME/.ssh/id_rsa.pub 
+	geeknote find "Blue Key (public)"
+	geeknote show 1 | tail -n 2 | head -n 1 > $HOME/.ssh/id_rsa.pub 
 	echo "Fetched RSA public key"
 else
 	echo "RSA private key already present"
 fi
-
-
